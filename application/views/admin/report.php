e67a4d07b847d313c01ce3f774dbe608cd7652a8
velia <velia.noviyanti88@gmail.com>
2021-06-13 15:49:47 +0700

    Changing report display

--- application/views/admin/report.php
+++ application/views/admin/report.php
@@ -1,32 +1,48 @@
-<div class="right_col" role="main">
-    <div class="row tile_count">
-    </div>
+<div class="right_col" role="">
 
-    <div class="row top_tiles">
-        <div class="animated flipInY col-lg-4 col-md4 col-sm-6 col-xs-12">
-            <div class="tile-stats">
+    <div class="" style="margin-top: 80px">
+        <div class="col-md-6">
+            <div class="tile-stats" style="padding: 15px 0 20px 10px">
                 <h3>Cetak Laporan</h3>
+                <br>    
 
-                <form action="<?php echo base_url(); ?>admin/pegawai/tambah" method="post">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-1 col-xs-12">Dari tanggal</label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                            </div>
+                <form class="form-horizontal" action="" method="POST">
+                    <div class="from-group" style="margin-top: 10px;">
+                        <label for="from_date" class="control-label col-md-4">Dari Tanggal</label>
+                        <div class="col-md-5" style="margin-bottom: 20px">
+                            <input type="date" class="form-control" id="from_date" name="from_date" required>
                         </div>
                     </div>
 
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-1 col-xs-12">Sampai tanggal</label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                            </div>
+                    <div class="form-group">
+                        <label for="to_date" class="control-label col-md-4">Sampai Tanggal</label>
+                        <div class="col-md-5">
+                            <input type="date" class="form-control" id="to_date" name="to_date" required>
                         </div>
                     </div>
+<!--                     
+                    <div class="form-group">
+                        <div class="col-sm-4" style="float: right;">
+                            <br><br>
+                            &emsp;&emsp;&emsp;&emsp;&emsp;
+                            <button class="btn btn-success" name="submit" type="submit" style="margin-right: 5px;">
+                                <i class="fa fa-save"></i> Save</button>
+
+                            <a href="<?php echo base_url(); ?>admin/listStudent" class="btn btn-danger" name="reset" type="reset">
+                                <i class="fa fa-times"></i> Back</a>
+                        </div>
+                    </div> -->
+                    <!-- <div class="form-group"> -->
+                        <!-- <div style="float: right;"> -->
+                    <button type="submit" class="btn btn-success" style="float:right; margin-right: 138px; margin-top: 30px;" target="_blank">Create Report</button>
+                    <!-- </div> -->
+                    <!-- </div> -->
+                </div>
                 </form>
 
             </div>
         </div>
     </div>
-
 </div>
+
+
